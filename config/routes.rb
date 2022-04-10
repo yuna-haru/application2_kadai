b08870d5769ec47f029428d967094b98002c143b
yuna-haru <skywar.love22@gmail.com>
2022-04-10 04:02:43 +0000

    kadai3 20syou

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   get 'homes/about' => 'homes#about', as:'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
