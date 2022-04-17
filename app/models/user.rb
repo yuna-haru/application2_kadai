313fbcb52d564ff6faf07bd51b71efa02bb569c3
yuna-haru <skywar.love22@gmail.com>
2022-04-17 12:57:55 +0000

    rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
 has_one_attached :profile_image
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 def get_profile_image(width,height)
