1edd3e468de3bc08bf8da5f53dabf64ac0aeb9a3
yuna-haru <skywar.love22@gmail.com>
2022-04-19 12:33:05 +0000

    rspec finish2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
-  get 'homes/about' => 'homes#about', as:'about'
+  get 'home/about' => 'homes#about', as:'about'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
